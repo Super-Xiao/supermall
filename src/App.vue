7dd77cd7b7432f487907df13deeef85088d5fa8a
Super-Xiao <1446751909@qq.com>
2021-06-30 21:58:18 +0800

    更新

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
-  <div id="app">
-  </div>
+  <div id="app"></div>
 </template>
 
+<script>
+export default {
+  
+}
+</script>
+
 <style>
-
-
-
+@import './assets/css/nomalize.css';
 </style>
