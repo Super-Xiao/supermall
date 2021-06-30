b4ea641a5d71caf8f20f8d6a49abb599fe58a4ca
Super-Xiao <1446751909@qq.com>
2021-06-30 23:01:23 +0800

    updata

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,15 @@
 </template>
 
 <script>
+import MainTabBar from'./components/content/mainTabbar/MainTabBar.vue'
 export default {
-  
+  name:'app',
+  components:{
+    MainTabBar
+  }
 }
 </script>
 
 <style>
-@import './assets/css/nomalize.css';
+@import './assets/css/normalize.css';
 </style>
---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="tab-bar">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBar",
+};
+</script>
+
+<style>
+#tab-bar {
+  background-color: #f6f6f6;
+  display: flex;
+  /* justify-content: space-between; */
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  box-shadow: 0px -2px 1px rgba(100, 100, 100, 0.1);
+}
+
+</style>
